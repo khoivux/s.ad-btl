d975b1cdf3d6c8df8de59fade3596398736d8925
Vũ Trọng Khôi <khoivux21@gmail.com>
2026-06-11 22:53:27 +0700

    Update settings.py

--- recommender-ai-service/recommender_ai_service/settings.py
+++ recommender-ai-service/recommender_ai_service/settings.py
@@ -86,4 +86,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', 'AIzaSyAv1FGyRYCuvASvWurdtB_ZHsBU8MgJ4Fw') # Cung cấp qua biến môi trường hoặc file .env
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', '') # Cung cấp qua biến môi trường hoặc file .env
